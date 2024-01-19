54f3d6f82f80df4ef82c6b770fa72caff0691c21
Ahmed Makram <154257919+AhmedMakram123@users.noreply.github.com>
2024-01-19 08:27:06 +0200

    Update sp_1-8.sql

--- DataBase/sp_1-8.sql
+++ DataBase/sp_1-8.sql
@@ -24,11 +24,11 @@ EXEC sp_UpdateCategory 10, 'Mac'
 ----------------------------------
 --3- Delete Category
 ----------------------------------
-CREATE OR ALTER PROCEDURE sp_DeleteCategory @Name NVARCHAR(50)
+CREATE OR ALTER PROCEDURE sp_DeleteCategory @Id int
 WITH ENCRYPTION
 AS
 	 Delete from Categories
-	where [CategoryName]= @Name
+	where [Id]= @Id 
 
 EXEC sp_DeleteCategory 'Mac'
 
@@ -65,7 +65,7 @@ EXEC sp_GetCategoryByName 'Mobile'
 ----------------------------------
 --7- Create Product
 ----------------------------------
-CREATE OR ALTER PROCEDURE sp_CreateProduct @Name NVARCHAR(50) ,@Quantity int,@Price Money,@Description NVARCHAR(50),@CategoryId INT
+CREATE OR ALTER PROCEDURE sp_CreateProduct @Name NVARCHAR(50) ,@Quantity int,@Price float,@Description NVARCHAR(50),@CategoryId INT
 WITH ENCRYPTION
 AS
 	INSERT INTO Products
@@ -76,7 +76,7 @@ EXEC sp_CreateProduct 'infinex',20,4000,'ssssssssssssssssssss',2
 ----------------------------------
 --8- Update Product
 ----------------------------------
-CREATE OR ALTER PROCEDURE sp_UpdateProduct @Name NVARCHAR(50) ,@Quantity int,@Price Money,@Description NVARCHAR(50),@CategoryId INT
+CREATE OR ALTER PROCEDURE sp_UpdateProduct @Id int, @Name NVARCHAR(50) ,@Quantity int,@Price Money,@Description NVARCHAR(50),@CategoryId INT
 WITH ENCRYPTION
 AS
 	Update Products
@@ -84,6 +84,6 @@ AS
 	, [Price]=@Price
 	,[Description]=@Description,
 	[CategoryId]=@CategoryId 
-	where [ProductName]=@Name
+	where [Id]=@Id 
 
 EXEC sp_UpdateProduct 'infinex',30,5000,'aaaaaaaaaaaaaaaaaaaaaa',2
